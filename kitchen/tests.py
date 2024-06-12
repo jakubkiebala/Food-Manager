d17ae7f7535c7274d3dcd002281f14788a7a22bb
Jakub Kiebala <kiebala.jakub@gmail.com>
2024-06-12 13:08:50 +0200

    added tests to project

--- kitchen/tests.py
+++ kitchen/tests.py
@@ -1,3 +1,12 @@
-from django.test import TestCase
+import pytest
+from django.test import TestCase, Client
+from django.urls import reverse
 
 # Create your tests here.
+
+
+def test_home_view():
+    url = reverse('base')
+    client = Client()
+    response = client.get(url)
+    assert response.status_code == 200
--- kitchen/views.py
+++ kitchen/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views import View
+
 
 # Create your views here.
+
+
+class SupplyManagerView(View):
+    def get(self, request):
+        return render(request, '')
