457ec3f4f39c988d99d2b6062ae50c503abd22c2
Jakub Kiebala <kiebala.jakub@gmail.com>
2024-06-12 12:43:48 +0200

    added title to home page

--- FOOD_MANAGER/urls.py
+++ FOOD_MANAGER/urls.py
@@ -20,5 +20,5 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', TemplateView.as_view(template_name='base.html'), name='base'),
+    path('', TemplateView.as_view(template_name='base/base.html'), name='base'),
 ]
